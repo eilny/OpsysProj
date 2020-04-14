bd6d6508364f83b8263080005b842bf56da5d2dd
Tom Saad <tomsaad5739@gmail.com>
2020-04-14 17:28:45 -0400

    simulationframework.cpp: adds main() - reads args in, declares deques for CPU/IO/WAIT process.*: process class

---
+++ process.h
@@ -0,0 +1,16 @@
+#include <stdint.h>
+
+#ifndef _H_PROCESS_H_
+#define _H_PROCESS_H_
+
+class process {
+    private:
+        unsigned int process_ID;
+        unsigned int arrival_time;
+        unsigned int processing_time;
+        unsigned int processed_time;
+
+        unsigned int turnaround_time;
+        unsigned int wait_time;
+
+#endif
---
+++ simulationframework.cpp
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <deque>
+
+int main( int argc, char ** argv) {
+    /* Determine what scheduling algorithm is used:
+       FCFS
+       SJF
+       SRT
+       RR
+     */
+
+    if (argc < 9) {
+        fprintf(stderr, "USAGE: ./a.out <seed> <lambda> <upper bound> < number of processes> \
+                <context switch time (even)> <alpha > <RR time slice> <RR [BEGINNING|END]>\n");
+        return EXIT_FAILURE;
+    }
+
+    // Submitty:
+    setvbuf( stdout, NULL, _IONBF, 0 );
+
+    // argv[1] - seed
+    int seed = atoi(argv[1]);
+    // argv[2] - lamda (interval times)
+    int lambda = atoi(argv[2]);
+    // argv[3] - upper bound
+    unsigned int upperbound = atoi(argv[3]);
+    // argv[4] - number of processes to simulate
+    unsigned int nproc = atoi(argv[4]);
+    // argv[5] - context switch time (ms) - expects even value
+    unsigned int tcs = atoi(argv[5]);
+    // argv[6] - alpha (cpu burst time estimate)
+    unsigned int alpha = atoi(argv[6]);
+    // argv[7] - RR timeslice
+    unsigned int timeslice = atoi(argv[7]);
+    // argv[8] - rradd: place processes at beginning or end of queue
+    unsigned int rraddbgn;
+    if (0 == strcmp("BEGINNING", argv[8])) {
+        rraddbgn = 1;
+    } else if (0 == strcmp("END", argv[8])) {
+        rraddbgn = 0;
+    } else {
+        fprintf(stderr, "USAGE: ./a.out <seed> <lambda> <upper bound> < number of processes> \
+                <context switch time (even)> <alpha > <RR time slice> <RR [BEGINNING|END]>\n");
+        return EXIT_FAILURE;
+    }
+
+    fprintf(stdout, "args:  seed: %d lambda: %d upper bound: %u nproc: %u tcs: %u alpha: %u rrtimeslice: %u rr begin: %u %s\n"
+            , seed
+            , lambda
+            , upperbound
+            , nproc
+            , tcs
+            , alpha
+            , timeslice
+            , rraddbgn
+            , (rraddbgn ? "BEGINNING" : "END") );
+
+
+
+    // set up queues
+    std::deque<int> READY;
+    std::deque<int> RUNNING;
+    std::deque<int> BLOCKED;
+
+
+    /* Run simulations
+     */
+
+}
